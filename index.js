8ed1e643a77b794b595a313d5fc1100fef507577
Rubens Gomes <vitorr_gomes@hotmail.com>
2017-10-18 23:14:34 -0200

    try another

--- index.js
+++ index.js
@@ -10,26 +10,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Add this middleware in your express app
 app.use(function (req, res, next) {
-
-	// Website you wish to allow to connect
-	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-
-	res.setHeader('Access-Control-Allow-Origin', process.env.PORT);
-
-	// Request methods you wish to allow
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-	// Request headers you wish to allow
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-	// Set to true if you need the website to include cookies in the requests sent
-	// to the API (e.g. in case you use sessions)
-	res.setHeader('Access-Control-Allow-Credentials', true);
-
-	// Pass to next layer of middleware
+	res.header('access-Control-Allow-Origin', '*');
 	next();
-});
+	});
 
 app.listen(process.env.PORT || 8080);
 
