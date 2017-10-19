7e55bf442f5f08756c169f95e782b4a6cdbcaa2e
Rubens Gomes <vitorr_gomes@hotmail.com>
2017-10-18 23:16:58 -0200

    other cors

--- index.js
+++ index.js
@@ -2,22 +2,15 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const app = express();
+const cors= require('./cors');
 
 const mountRoutes = require('./server/routes');
 mountRoutes(app);
 
 app.use(logger('dev'));
+app.use(cors.permission);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-//Add this middleware in your express app
-app.use(function (req, res, next) {
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-	res.header('access-Control-Allow-Origin', '*');
-	next();
-	});
-
 app.listen(process.env.PORT || 8080);
 
 console.log('Server Listening!');
