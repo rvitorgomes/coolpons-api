a38fefc09673c9bb9086d3706088374182c8c564
Rubens Gomes <vitorr_gomes@hotmail.com>
2017-10-18 22:29:23 -0200

    start a diff project for backend

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+
+const mountRoutes = require('./server/routes');
+mountRoutes(app);
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(process.env.PORT || 8080);
+
+console.log('Server Listening!');
+
+module.exports = app;
