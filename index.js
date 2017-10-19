24adbe742dd738cf4e6106df2b652642131b3d07
Rubens Gomes <vitorr_gomes@hotmail.com>
2017-10-18 23:30:32 -0200

    trying another way

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const app = express();
-const cors= require('./cors');
+const cors= require('cors');
 
 const mountRoutes = require('./server/routes');
 mountRoutes(app);
@@ -10,10 +10,22 @@ mountRoutes(app);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors.permission);
-app.use(cors({
-    allowedHeaders: ["Content-Type", "Authorization"]
-}));
+
+const originsWhitelist = [
+	'http://localhost:4200',      //this is my front-end url for development
+	'https://coolpons-api.herokuapp.com/'
+];
+
+const corsOptions = {
+	origin: function(origin, callback){
+		const isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
+		callback(null, isWhitelisted);
+	},
+	credentials:true
+}
+  //here is the magic
+app.use(cors(corsOptions));
+
 app.listen(process.env.PORT || 8080);
 
 console.log('Server Listening!');
