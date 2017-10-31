61fae3999262804c23ec28867366ad528dc317ce
Rubens Gomes <vitorr_gomes@hotmail.com>
2017-10-31 00:04:07 -0200

    change db and create product endpoint

--- server/db.js
+++ server/db.js
@@ -1,6 +1,8 @@
 const { Pool, Client } = require('pg');
 
-const connectionString = 'postgres://xawaeirdkqxqcz:f31a24ff510a85fb6e151aaaf811f05d3ade7d84a92a77e06e5148f62c0a856b@ec2-23-23-248-247.compute-1.amazonaws.com:5432/dgtlb9gh3uvkg';
+// const connectionStringOld = 'postgres://xawaeirdkqxqcz:f31a24ff510a85fb6e151aaaf811f05d3ade7d84a92a77e06e5148f62c0a856b@ec2-23-23-248-247.compute-1.amazonaws.com:5432/dgtlb9gh3uvkg';
+
+const connectionString = 'postgres://dypezzejmngwim:b888b97ae4782aa3dc9f9d1318f30896823a5c7e9689718dbc963bfeabe13526@ec2-54-235-72-121.compute-1.amazonaws.com:5432/d9kob3f4nl9h56';
 
 const pool = new Pool({
   connectionString: connectionString,
--- server/routes/products.js
+++ server/routes/products.js
@@ -10,14 +10,24 @@ const router = new Router();
 // export our router to be mounted by the parent application
 module.exports = router;
 
+// GET ALL products
 router.get('/', async (req, res) => {
-	const { rows } = await db.query('SELECT * FROM produtos')
-	res.send(rows)
+	const { rows } = await db.query('SELECT * FROM products ORDER BY name ASC');
+	return res.send(rows);
 });
 
+// GET product
 router.get('/:id', async (req, res) => {
 	const { id } = req.params;
-	const { rows } = await db.query('SELECT * FROM produtos WHERE cod_prod = $1', [id]);
-	res.send(rows[0]);
+	const { rows } = await db.query('SELECT * FROM products WHERE id = $1', [id]);
+	return res.send(rows[0]);
+});
+
+// POST product
+router.post('/', async (req, res) => {
+	const data = req.body;
+	const { rows } = await db.query('INSERT INTO products VALUES($1, $2, $3, $4,  $5, $6, $7, $8, $9) RETURNING *',
+		[data.id, data.name, data.picture, data.price, data.expiration_date, data.type, data.company, data.price_discount, data.status]);
+	return res.send(rows);
 });
 
